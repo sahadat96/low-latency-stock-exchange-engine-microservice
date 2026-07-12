ad738daf393c1399498f213b8fba05fb8c063f5f
sahadat96 <sahadatappdev@gmail.com>
2026-07-12 09:49:10 +0600

    Feat(ApiKey): Create api key controller

---
+++ user/src/modules/api-key/presentation/controller/api-key.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Ip,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+
+import { CreateApiKeyDto } from '../dto/api-key.dto';
+import { CreateApiKeyResponseDto } from '../dto/api-key.response.dto';
+
+@Controller('api-keys')
+export class ApiKeyController {
+  constructor(private readonly apiKeyService: ApiKeyService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async createApiKey(
+    @CurrentUser() user: JwtPayload,
+    @Body() dto: CreateApiKeyDto,
+    @Ip() ip: string,
+  ): Promise<CreateApiKeyResponseDto> {
+    return this.apiKeyService.createApiKey(user.sub, dto, ip);
+  }
+}
