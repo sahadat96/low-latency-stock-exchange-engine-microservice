8dba293c00fb9ebd60c6ee03b5d4e70cadaed12c
sahadat96 <sahadatappdev@gmail.com>
2026-08-22 23:18:16 +0600

    Feat(Kafka Module): Create kafka config

---
+++ user/src/modules/kafka-module/kafka.config.ts
@@ -0,0 +1,21 @@
+import { registerAs } from '@nestjs/config';
+
+export interface KafkaConfig {
+  clientId: string;
+  brokers: string[];
+}
+
+export default registerAs(
+  'kafka',
+  (): KafkaConfig => ({
+    clientId:
+      process.env.KAFKA_CLIENT_ID ?? 'user-service',
+
+    brokers: (
+      process.env.KAFKA_BROKERS ?? 'localhost:9092'
+    )
+      .split(',')
+      .map((broker) => broker.trim())
+      .filter(Boolean),
+  }),
+);
