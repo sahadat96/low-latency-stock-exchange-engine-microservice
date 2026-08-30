58cccf0cb74b80b62f6662ab4eb4ca9204137e4e
sahadat96 <sahadatappdev@gmail.com>
2026-08-30 16:23:21 +0600

    Feat(Wallet Service): Create wallet controller

---
+++ wallet-service/src/modules/wallet/presentation/controller/wallet.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+
+import { WalletService } from '../../application/wallet.service';
+
+import { WalletDto } from '../dto/wallet.dto';
+
+import {
+  CurrentUser,
+} from '@/common/decorators/current.user.decorato';
+
+import type {
+  JwtPayload,
+} from '@/common/decorators/current.user.decorato';
+
+@Controller('wallets')
+export class WalletController {
+  constructor(
+    private readonly walletService: WalletService,
+  ) {}
+  
+  @Get('me')
+  @HttpCode(HttpStatus.OK)
+  async getMyWallet(
+    @CurrentUser() user: JwtPayload,
+  ): Promise<WalletDto> {
+    return this.walletService.getMyWallet(
+      user.sub,
+    );
+  }
+}
+
