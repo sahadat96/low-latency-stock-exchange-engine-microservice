575df147b4f6c62f2f2730219d80be95c40456d8
sahadat96 <sahadatappdev@gmail.com>
2026-09-02 16:51:09 +0600

    Feat(Wallet Service): Create transaction module and imported into app.module

--- wallet-service/src/app.module.ts
+++ wallet-service/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletModule } from './modules/wallet/wallet.module';
+import { TransactionModule } from './modules/transection/transaction.module';
 
 @Module({
   imports: [
     WalletModule,
+    TransactionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ wallet-service/src/modules/transection/transaction.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+import { TransactionController } from './presentation/controller/transaction.controller';
+
+import { TransactionService } from './application/transaction.service';
+
+import {
+  TRANSACTION_REPOSITORY,
+} from './domain/interfaces/transaction.repository.interface';
+
+import {
+  TransactionRepository,
+} from './infrastructure/repositories/transaction.repository';
+
+@Module({
+  imports: [],
+
+  controllers: [
+    TransactionController,
+  ],
+
+  providers: [
+    TransactionService,
+    PrismaService,
+    {
+      provide: TRANSACTION_REPOSITORY,
+
+      useClass: TransactionRepository,
+    },
+  ],
+
+  exports: [
+    TransactionService,
+  ],
+})
+export class TransactionModule {}
